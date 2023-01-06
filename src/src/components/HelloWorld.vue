2349df6813b7b304d1a07528b60f099eb23bb28e
neomn110 <neomn110@gmail.com>
2023-01-06 20:46:03 +0330

    refactor: hole welcome thing cleared

--- src/src/components/HelloWorld.vue
+++ src/src/components/HelloWorld.vue
@@ -1,14 +1,8 @@
 <script setup>
-import { ref } from 'vue'
 
-const count = ref(0)
 </script>
 
 <template>
 
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-  </div>
 </template>
 
