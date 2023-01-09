b86a6e2000c6c912de443f6c3d4bd8b8643f5360
neomn110 <neomn110@gmail.com>
2023-01-09 16:50:55 +0330

    feat: creat postcss config file

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwind from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,3 +13,5 @@ export default defineConfig({
     }
   }
 })
+
+
