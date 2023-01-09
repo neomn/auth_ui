38a2ba0a449ec385fdb14e96bd2340419e7b2e17
neomn110 <neomn110@gmail.com>
2023-01-10 00:57:37 +0330

    style: position social media buttons

--- src/src/components/inputs.vue
+++ src/src/components/inputs.vue
@@ -2,7 +2,15 @@
 
 </script>
 <template>
-  <div >
+  <div class="flex flex-col justify-start items-center  overflow-hidden">
+    <input placeholder="   Email" class="basis-2/12 w-11/12 mt-2 mb-1 border rounded-lg bg-gray-700"/>
+    <input placeholder="   password" class="basis-2/12 w-11/12 mb-1 border rounded-lg bg-gray-700"/>
+    <div class="basis-5/12 self-center w-11/12 m-2 border rounded-xl">
 
+    </div>
+    <div class="basis-1/12 mb-1 flex items-center">
+      <input type="checkbox" class=""/>
+      <label for="" class="mx-2">Remember Me</label>
+    </div>
   </div>
 </template>
--- src/src/components/socialMediaLogin.vue
+++ src/src/components/socialMediaLogin.vue
@@ -1,5 +1,7 @@
 <template>
-  <div >
-
+  <div class="flex justify-around items-center">
+    <div class=" w-16 h-16 m-2  border border-gray-500 rounded-full"></div>
+    <div class=" w-16 h-16 m-2  border border-gray-500 rounded-full"></div>
+    <div class=" w-16 h-16 m-2  border border-gray-500 rounded-full"></div>
   </div>
 </template>
