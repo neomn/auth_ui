205e60cc5b632ce8e609e4587a71542dd58e1438
neomn110 <neomn110@gmail.com>
2023-01-10 15:23:55 +0330

    style: remove login border

--- src/src/components/inputs.vue
+++ src/src/components/inputs.vue
@@ -4,8 +4,8 @@
 <template>
   <div class="flex flex-col justify-start items-center  overflow-hidden">
     <input placeholder="   Email" class="basis-2/12 w-11/12 mt-2 mb-1 border rounded-lg bg-gray-700"/>
-    <input placeholder="   password" class="basis-2/12 w-11/12 mb-1 border rounded-lg bg-gray-700"/>
-    <div class="basis-5/12 self-center w-11/12 m-2 border rounded-xl">
+    <input placeholder="   Password" class="basis-2/12 w-11/12 mb-1 border rounded-lg bg-gray-700"/>
+    <div class="basis-5/12 self-center w-11/12 m-2 border border-gray-600 rounded-xl">
 
     </div>
     <div class="basis-1/12 mb-1 flex items-center">
--- src/src/components/login.vue
+++ src/src/components/login.vue
@@ -8,7 +8,7 @@ export default {
 </script>
 <template>
   <div class="flex flex-col m-2 mt-1 sm:m-2 sm:ml-1 border-2 border-gray-900 rounded-2xl text-white bg-gray-700">
-    <inputs class="basis-8/12 m-2 mb-1 border-2 border-gray-900 rounded-2xl"/>
+    <inputs class="basis-8/12"/>
     <SocialMediaLogin class="basis-3/12 sm:basis-2/12 m-2 mt-1 mb-1 border-2 border-gray-900 rounded-2xl"/>
     <Buttons class="basis-2/12 sm:basis-1/12 m-2 mt-1 border-2 border-gray-900 rounded-2xl"/>
   </div>
