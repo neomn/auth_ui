b7a9db7a99ae733b9eed0b9c4b0a3a9f7e327608
neomn110 <neomn110@gmail.com>
2023-01-10 16:50:39 +0330

    style: add login register forget-password buttons

--- src/src/components/buttons.vue
+++ src/src/components/buttons.vue
@@ -1,5 +1,16 @@
 <template>
-  <div >
-
+  <div class="flex justify-around">
+    <button class="border border-gray-400 ">
+      <img/>
+      <label>Login</label>
+    </button>
+    <button class="border border-gray-400 ">
+      <img/>
+      <label>Register</label>
+    </button>
+    <button class="border border-gray-400 ">
+      <img/>
+      <label>Forget Password?</label>?
+    </button>
   </div>
 </template>
