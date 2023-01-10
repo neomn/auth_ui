024c03e650fdf0897828464895bebb60526ed240
neomn110 <neomn110@gmail.com>
2023-01-10 15:26:22 +0330

    style: remove social media border

--- src/src/components/login.vue
+++ src/src/components/login.vue
@@ -9,7 +9,7 @@ export default {
 <template>
   <div class="flex flex-col m-2 mt-1 sm:m-2 sm:ml-1 border-2 border-gray-900 rounded-2xl text-white bg-gray-700">
     <inputs class="basis-8/12"/>
-    <SocialMediaLogin class="basis-3/12 sm:basis-2/12 m-2 mt-1 mb-1 border-2 border-gray-900 rounded-2xl"/>
+    <SocialMediaLogin class="basis-3/12 sm:basis-2/12 m-2 mt-1 mb-1"/>
     <Buttons class="basis-2/12 sm:basis-1/12 m-2 mt-1 border-2 border-gray-900 rounded-2xl"/>
   </div>
 </template>
