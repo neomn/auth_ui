a510a4942e73e4898e830dc7f6fc818f22479ea3
neomn110 <neomn110@gmail.com>
2023-01-10 20:11:43 +0330

    feat: add google and linked in icons

--- src/src/components/buttons.vue
+++ src/src/components/buttons.vue
@@ -5,7 +5,7 @@
 </script>
 
 <template>
-  <div class="flex justify-around text-sm text-gray-400">
+  <div class="flex justify-around text-sm text-gray-400 overflow-hidden">
     <button class="basis-1/3 m-1 flex flex-col items-center ">
       <img src="src/assets/icons/8983815.png" class="w-8"/>
       <label class=" ">Login</label>
