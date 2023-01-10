21b3ec824e97971be56f4a4c8b46033f7fe4e739
neomn110 <neomn110@gmail.com>
2023-01-10 16:16:05 +0330

    style: modfy positioning for sm size

--- src/src/components/login.vue
+++ src/src/components/login.vue
@@ -7,9 +7,9 @@ export default {
 }
 </script>
 <template>
-  <div class="flex flex-col m-2 mt-1 sm:m-2 sm:ml-1 border-2 border-gray-900 rounded-2xl text-white bg-gray-700">
+  <div class="flex flex-col sm:justify-center m-2 mt-1 sm:m-2 sm:ml-1 border-2 border-gray-900 rounded-2xl text-white bg-gray-700">
     <inputs class="basis-8/12"/>
-    <SocialMediaLogin class="basis-3/12 sm:basis-2/12 m-2 mt-1 mb-1"/>
-    <Buttons class="basis-2/12 sm:basis-1/12 border-t border-gray-800"/>
+    <SocialMediaLogin class="basis-3/12 sm:basis-2/12"/>
+    <Buttons class="basis-2/12 sm:basis-1/12 border-t border-gray-800 sm:border-none"/>
   </div>
 </template>
--- src/src/components/socialMediaLogin.vue
+++ src/src/components/socialMediaLogin.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-around items-center">
+  <div class="flex justify-around sm:justify-center items-center">
     <div class=" w-16 h-16 m-2  border border-gray-500 rounded-full"></div>
     <div class=" w-16 h-16 m-2  border border-gray-500 rounded-full"></div>
     <div class=" w-16 h-16 m-2  border border-gray-500 rounded-full"></div>
