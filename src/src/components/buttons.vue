38dc2a594eb0fde6f7ac72972c653dd3931c6656
neomn110 <neomn110@gmail.com>
2023-01-10 20:01:12 +0330

    style: buttons positioning done for mobile view

--- src/src/components/buttons.vue
+++ src/src/components/buttons.vue
@@ -6,17 +6,17 @@
 
 <template>
   <div class="flex justify-around text-sm text-gray-400">
-    <button class="basis-1/3 m-1 flex flex-col items-center border border-gray-400 ">
-      <img src="src/assets/icons/8983815.png" class="basis-2/3 border"/>
-      <label class="basis-1/3 border">Login</label>
+    <button class="basis-1/3 m-1 flex flex-col items-center ">
+      <img src="src/assets/icons/8983815.png" class="w-8"/>
+      <label class=" ">Login</label>
     </button>
-    <button class="basis-1/3 m-1 flex flex-col items-center border border-gray-400 ">
-      <img src="src/assets/icons/3631696.png" class="basis-2/3 border"/>
-      <label class="basis-1/3 border">Register</label>
+    <button class="basis-1/3 m-1 flex flex-col items-center">
+      <img src="src/assets/icons/3631696.png" class="w-8 "/>
+      <label class=" ">Register</label>
     </button>
-    <button class="basis-1/3 m-1 flex flex-col items-center border border-gray-400 ">
-      <img src="src/assets/icons/1745504.png" alt="" class="basis-2/3  border"/>
-      <label class="basis-1/3 border">Recover</label>
+    <button class="basis-1/3 m-1 flex flex-col items-center ">
+      <img src="src/assets/icons/1745504.png" alt="" class="w-8"/>
+      <label class=" ">Recover</label>
     </button>
   </div>
 </template>
