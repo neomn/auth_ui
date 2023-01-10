2c931a12b6f8d9cd2e19ff9474cb8163da6851ed
neomn110 <neomn110@gmail.com>
2023-01-10 15:35:33 +0330

    style: change buttons component border

--- src/src/components/login.vue
+++ src/src/components/login.vue
@@ -10,6 +10,6 @@ export default {
   <div class="flex flex-col m-2 mt-1 sm:m-2 sm:ml-1 border-2 border-gray-900 rounded-2xl text-white bg-gray-700">
     <inputs class="basis-8/12"/>
     <SocialMediaLogin class="basis-3/12 sm:basis-2/12 m-2 mt-1 mb-1"/>
-    <Buttons class="basis-2/12 sm:basis-1/12 m-2 mt-1 border-2 border-gray-900 rounded-2xl"/>
+    <Buttons class="basis-2/12 sm:basis-1/12 border-t border-gray-800"/>
   </div>
 </template>
