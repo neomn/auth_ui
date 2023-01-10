b07306fa0f904dffa28b9c732dd9480eae12629a
neomn110 <neomn110@gmail.com>
2023-01-10 20:21:15 +0330

    style: set social login icons grayscaale attribute

--- src/src/components/socialMediaLogin.vue
+++ src/src/components/socialMediaLogin.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-around sm:justify-center items-center">
-    <div class=" w-16 h-16 m-2  border border-gray-500 rounded-full"></div>
-    <div class=" w-16 h-16 m-2  border border-gray-500 rounded-full"></div>
-    <div class=" w-16 h-16 m-2  border border-gray-500 rounded-full"></div>
+    <button class=" w-16 h-16 m-2 grayscale hover:grayscale-0 rounded-full"><img src="src/assets/icons/300221.png" alt=""></button>
+    <button class=" w-16 h-16 m-2 grayscale hover:grayscale-0 rounded-full"><img src="src/assets/icons/145807.png" alt=""></button>
+    <button class=" w-16 h-16 m-2 grayscale hover:grayscale-0 rounded-full"><img src="src/assets/icons/300221.png" alt=""></button>
   </div>
 </template>
