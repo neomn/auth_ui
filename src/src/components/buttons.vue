2817129d6179d58c3ddb3a5c1a6bd9354b458e5b
neomn110 <neomn110@gmail.com>
2023-01-11 14:36:36 +0330

    style: remove unnecessary labels

--- src/src/components/buttons.vue
+++ src/src/components/buttons.vue
@@ -6,17 +6,17 @@
 
 <template>
   <div class="flex justify-around sm:justify-evenly items-center text-sm text-gray-400 overflow-hidden">
-    <button class="basis-1/3 sm:basis-1/6 m-1 flex flex-col items-center p-1">
+    <button class="basis-1/3 sm:basis-1/6 m-1 flex flex-col items-center p-1 border">
       <img src="src/assets/icons/8983815.png" class="w-6"/>
-      <label class=" ">Login</label>
+      Login
     </button>
     <button class="basis-1/3 sm:basis-1/6 m-1 flex flex-col items-center p-1">
       <img src="src/assets/icons/3631696.png" class="w-6 "/>
-      <label class=" ">Register</label>
+      Register
     </button>
     <button class="basis-1/3 sm:basis-1/6 m-1 flex flex-col items-center p-1">
       <img src="src/assets/icons/1745504.png" alt="" class="w-6"/>
-      <label class=" ">Recover</label>
+      Recover
     </button>
   </div>
 </template>
