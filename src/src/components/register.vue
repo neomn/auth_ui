56bc44f9122f1e10b1d9cb645808de611f8fa449
neomn110 <neomn110@gmail.com>
2023-01-11 19:47:32 +0330

    refactor: copy login tmplate into register component

--- src/src/components/register.vue
+++ src/src/components/register.vue
@@ -1,5 +1,15 @@
 <template>
+  <div class="flex flex-col justify-start sm:justify-end items-center overflow-hidden">
+    <input placeholder="   Email" class="basis-2/12 sm:basis-1/12 w-11/12 sm:w-3/4 mt-2 mb-1 border border-gray-500 rounded-lg bg-gray-700"/>
+    <input placeholder="   Password" class="basis-2/12 sm:basis-1/12 w-11/12 sm:w-3/4 mb-1 border border-gray-500 rounded-lg bg-gray-700"/>
+    <div class="basis-5/12 sm:basis-6/12 self-center w-11/12 sm:w-3/4 m-2 border border-gray-600 rounded-xl">
 
+    </div>
+    <div class="basis-1/12 mb-1 flex items-center">
+      <input type="checkbox" class=""/>
+      <label for="" class="mx-2 text-gray-400">Remember Me</label>
+    </div>
+  </div>
 </template>
 
 <script>
