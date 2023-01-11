1766c7068d5d99a37165c248f2be8dfb01aacad9
neomn110 <neomn110@gmail.com>
2023-01-11 14:31:42 +0330

    style: remove unnecessary border

--- src/src/components/buttons.vue
+++ src/src/components/buttons.vue
@@ -5,7 +5,7 @@
 </script>
 
 <template>
-  <div class="flex justify-around sm:justify-evenly items-center text-sm text-gray-400 overflow-hidden border">
+  <div class="flex justify-around sm:justify-evenly items-center text-sm text-gray-400 overflow-hidden">
     <button class="basis-1/3 sm:basis-1/6 m-1 flex flex-col items-center p-1">
       <img src="src/assets/icons/8983815.png" class="w-6"/>
       <label class=" ">Login</label>
