adbbc33ec3abcb2928aa2f67907e44e07de0492f
neomn110 <neomn110@gmail.com>
2023-01-11 14:09:55 +0330

    style: set buttons basis in sm size

--- src/src/components/buttons.vue
+++ src/src/components/buttons.vue
@@ -6,15 +6,15 @@
 
 <template>
   <div class="flex justify-around sm:justify-evenly text-sm text-gray-400 overflow-hidden border">
-    <button class="basis-1/3 m-1 flex flex-col items-center p-1 border border-white">
+    <button class="basis-1/3 sm:basis-1/6 m-1 flex flex-col items-center p-1 border border-white">
       <img src="src/assets/icons/8983815.png" class="w-8"/>
       <label class=" ">Login</label>
     </button>
-    <button class="basis-1/3 m-1 flex flex-col items-center p-1">
+    <button class="basis-1/3 sm:basis-1/6 m-1 flex flex-col items-center p-1">
       <img src="src/assets/icons/3631696.png" class="w-8 "/>
       <label class=" ">Register</label>
     </button>
-    <button class="basis-1/3 m-1 flex flex-col items-center p-1">
+    <button class="basis-1/3 sm:basis-1/6 m-1 flex flex-col items-center p-1">
       <img src="src/assets/icons/1745504.png" alt="" class="w-8"/>
       <label class=" ">Recover</label>
     </button>
