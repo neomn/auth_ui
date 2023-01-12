41a6e77d2b302869f8f8ba2a524d4e8ef5bec7cc
neomn110 <neomn110@gmail.com>
2023-01-12 20:39:13 +0330

    refactor: rename login icon

--- src/src/components/socialMediaLogin.vue
+++ src/src/components/socialMediaLogin.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex justify-center sm:justify-center items-center">
-    <button class=" w-10 h-10 m-2 grayscale hover:grayscale-0 rounded-full"><img src="src/assets/icons/300221.png" alt=""></button>
+    <button class=" w-10 h-10 m-2 grayscale hover:grayscale-0 rounded-full"><img src="src/assets/icons/login.png" alt=""></button>
     <button class=" w-10 h-10 m-2 grayscale hover:grayscale-0 rounded-full"><img src="src/assets/icons/145807.png" alt=""></button>
     <button class=" w-10 h-10 m-2 grayscale hover:grayscale-0 rounded-full"><img src="src/assets/icons/463816.png" alt=""></button>
     <button class=" w-10 h-10 m-2 grayscale hover:grayscale-0 rounded-full"><img src="src/assets/icons/icon_email.png" alt=""></button>
