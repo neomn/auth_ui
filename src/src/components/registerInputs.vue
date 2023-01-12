e92988ac3156d5838d56cf4cf0480a7c46147765
neomn110 <neomn110@gmail.com>
2023-01-12 19:52:27 +0330

    feat: add email icon

--- src/src/components/registerInputs.vue
+++ src/src/components/registerInputs.vue
@@ -1,11 +1,8 @@
 <template>
-  <div class="flex flex-col justify-start sm:justify-end items-center overflow-hidden">
+  <div class="flex flex-col justify-evenly sm:justify-end items-center overflow-hidden">
     <input placeholder="   Email" class="basis-2/12 sm:basis-1/12 w-11/12 sm:w-3/4 mt-2 mb-1 border border-gray-500 rounded-lg bg-gray-700"/>
     <input placeholder="   Password" class="basis-2/12 sm:basis-1/12 w-11/12 sm:w-3/4 mb-1 border border-gray-500 rounded-lg bg-gray-700"/>
     <input placeholder="   Confirm Password" class="basis-2/12 sm:basis-1/12 w-11/12 sm:w-3/4 mb-1 border border-gray-500 rounded-lg bg-gray-700"/>
-    <div class="basis-5/12 sm:basis-6/12 self-center w-11/12 sm:w-3/4 m-2 border border-gray-600 rounded-xl">
-
-    </div>
   </div>
 </template>
 
