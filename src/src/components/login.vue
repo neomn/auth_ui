43e19779da93fe8343abb0b0fa78a286b3b2a4a3
neomn110 <neomn110@gmail.com>
2023-01-12 19:41:21 +0330

    style: set basis

--- src/src/components/login.vue
+++ src/src/components/login.vue
@@ -10,7 +10,7 @@ export default {
 <template>
   <div class="flex flex-col sm:justify-center m-2 mt-1 sm:m-2 sm:ml-1 border-2 border-gray-900 rounded-2xl text-white bg-gray-700">
     <loginInputs v-if="false" class="basis-8/12"/>
-    <RegisterInputs v-if="true" class="border"/>
+    <RegisterInputs v-if="true" class="basis-8/12 border"/>
     <SocialMediaLogin class="basis-3/12 sm:basis-2/12"/>
     <Buttons class="basis-2/12 sm:basis-2/12 border-t border-gray-800 sm:border-none"/>
   </div>
