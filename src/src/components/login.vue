37fc669639672f019343264456d67b70af979898
neomn110 <neomn110@gmail.com>
2023-01-12 19:10:56 +0330

    feat: add register inputs to the login component

--- src/src/components/login.vue
+++ src/src/components/login.vue
@@ -1,14 +1,16 @@
 <script>
-import loginInputs from "./loginInputs.vue";
+import LoginInputs from "./loginInputs.vue";
 import SocialMediaLogin from "./socialMediaLogin.vue";
+import RegisterInputs from "./registerInputs.vue";
 import Buttons from "./buttons.vue"
 export default {
-  components: {SocialMediaLogin, loginInputs, Buttons}
+  components: {SocialMediaLogin, LoginInputs, RegisterInputs, Buttons}
 }
 </script>
 <template>
   <div class="flex flex-col sm:justify-center m-2 mt-1 sm:m-2 sm:ml-1 border-2 border-gray-900 rounded-2xl text-white bg-gray-700">
     <loginInputs class="basis-8/12"/>
+    <RegisterInputs/>
     <SocialMediaLogin class="basis-3/12 sm:basis-2/12"/>
     <Buttons class="basis-2/12 sm:basis-2/12 border-t border-gray-800 sm:border-none"/>
   </div>
