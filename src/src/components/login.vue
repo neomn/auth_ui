b58b08c80ae55beef03c65338d760c6a48ce1912
neomn110 <neomn110@gmail.com>
2023-01-12 19:04:10 +0330

    refactor: rename inputs to loginInputs

--- src/src/components/login.vue
+++ src/src/components/login.vue
@@ -1,14 +1,14 @@
 <script>
-import Inputs from "./inputs.vue";
+import loginInputs from "./loginInputs.vue";
 import SocialMediaLogin from "./socialMediaLogin.vue";
 import Buttons from "./buttons.vue"
 export default {
-  components: {SocialMediaLogin, Inputs, Buttons}
+  components: {SocialMediaLogin, loginInputs, Buttons}
 }
 </script>
 <template>
   <div class="flex flex-col sm:justify-center m-2 mt-1 sm:m-2 sm:ml-1 border-2 border-gray-900 rounded-2xl text-white bg-gray-700">
-    <inputs class="basis-8/12"/>
+    <loginInputs class="basis-8/12"/>
     <SocialMediaLogin class="basis-3/12 sm:basis-2/12"/>
     <Buttons class="basis-2/12 sm:basis-2/12 border-t border-gray-800 sm:border-none"/>
   </div>
