d9fc4c0719f39ec664add749c20cfe0b68b5ad7d
neomn110 <neomn110@gmail.com>
2023-01-14 07:51:38 +0330

    feat: create routes.js

--- src/src/main.js
+++ src/src/main.js
@@ -2,4 +2,15 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+
+const routes = [
+    {path: '/', component: App}
+]
+
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+
+})
+
+App.use(router)
 createApp(App).mount('#app')
