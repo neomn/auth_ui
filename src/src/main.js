82bd49bb44b1ed58eb294c425d69fa57a17886a3
neomn110 <neomn110@gmail.com>
2023-01-14 08:22:26 +0330

    feat: define some route

--- src/src/main.js
+++ src/src/main.js
@@ -2,15 +2,4 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-
-const routes = [
-    {path: '/', component: App}
-]
-
-const router = VueRouter.createRouter({
-    history: VueRouter.createWebHashHistory(),
-
-})
-
-App.use(router)
 createApp(App).mount('#app')
---
+++ src/src/routes.js
@@ -0,0 +1,9 @@
+import login from './components/'
+import register from './components/'
+import recover from './components/'
+
+const routes = [
+    {path: '/login', name:'login', component: root},
+    {path: '/register', name:'register', component: root},
+    {path: '/recover', name:'recover', component: root},
+]
