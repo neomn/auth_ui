44b1af07e0e46604e7447b9ef2a0d005a9d49ba2
neomn110 <neomn110@gmail.com>
2023-01-14 10:41:03 +0330

    feat: add register and recover routes

--- src/src/routes.js
+++ src/src/routes.js
@@ -1,11 +1,11 @@
 import login from './components/login.vue'
-// import register from './components/'
-// import recover from './components/'
+import register from './components/register.vue'
+import recover from './components/recover.vue'
 
 const routes = [
     {path: '/login', name:'login', component: login},
-    // {path: '/register', name: 'register', component: register},
-    // {path: '/recover', name:'recover', component: root},
+    {path: '/register', name: 'register', component: register},
+    {path: '/recover', name:'recover', component: recover},
 ]
 
 export {routes}
