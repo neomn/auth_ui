15c2eb63c85a88714eb42a0b648dcf570b283e61
neomn110 <neomn110@gmail.com>
2023-01-14 10:26:49 +0330

    refactor: load routes from an external file (routes.js)

--- src/src/main.js
+++ src/src/main.js
@@ -1,17 +1,11 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
-// import { routes } from './routes'
+import { routes } from './routes'
 import App from './App.vue'
-import login from "./components/login.vue";
-
-const Routes = [{path: '/login', name:'login', component: login},
-    // {path: '/register', name:'register', component: root},
-    // {path: '/recover', name:'recover', component: root},
-    ]
 
 const router = createRouter({
     history: createWebHistory(),
-    routes: Routes,
+    routes: routes,
 })
 
 const app = createApp(App)
--- src/src/routes.js
+++ src/src/routes.js
@@ -2,10 +2,10 @@ import login from './components/login.vue'
 // import register from './components/'
 // import recover from './components/'
 
-export function routes() {
-        return [
-            {path: '/login', name:'login', component: login},
-            // {path: '/register', name:'register', component: root},
-            // {path: '/recover', name:'recover', component: root},
-        ]
-}
+const routes = [
+    {path: '/login', name:'login', component: login},
+    // {path: '/register', name: 'register', component: register},
+    // {path: '/recover', name:'recover', component: root},
+]
+
+export {routes}
