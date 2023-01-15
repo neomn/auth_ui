1aef05e68b66aba61bffee1ff708f6ca3b55282d
neomn110 <neomn110@gmail.com>
2023-01-15 10:20:43 +0330

    style: adjust height

--- src/src/components/login.vue
+++ src/src/components/login.vue
@@ -2,9 +2,9 @@
   <div class="flex flex-col sm:flex-row bg-gray-600">
     <WebsiteLogo class="basis-4/12 sm:basis-1/2 m-2 mb-1 sm:mb-2 "/>
     <div class="basis-8/12 sm:basis-1/2 m-2 mt-1 sm:mt-2 border-2 border-gray-900 rounded-2xl overflow-hidden ">
-      <loginInputs class="h-2/4"/>
-      <SocialMediaLogin class="h-1/4"/>
-      <Buttons class="h-1/4"/>
+      <loginInputs class="h-4/6"/>
+      <SocialMediaLogin class="h-1/6"/>
+      <Buttons class="h-1/6"/>
     </div>
   </div>
 </template>
