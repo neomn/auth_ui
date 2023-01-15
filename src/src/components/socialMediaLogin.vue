d273230b75ad17ba2238d3bb5279328e4e965484
neomn110 <neomn110@gmail.com>
2023-01-15 19:48:17 +0330

    refactor: only one item in social media login can be selected

--- src/src/components/socialMediaLogin.vue
+++ src/src/components/socialMediaLogin.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex justify-center sm:justify-center items-center bg-gray-800">
-    <button class=" w-10 h-10 m-2 grayscale hover:grayscale-0 rounded-full"><img src="src/assets/icons/463816.png" alt=""></button>
-    <button class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(route === 'login')? '':'grayscale'" ><img src="src/assets/icons/email.png" alt=""></button>
-    <button class=" w-10 h-10 m-2 grayscale hover:grayscale-0 rounded-full"><img src="src/assets/icons/google.png" alt=""></button>
-    <button class=" w-10 h-10 m-2 grayscale hover:grayscale-0 rounded-full"><img src="src/assets/icons/145807.png" alt=""></button>
+    <button v-on:click="changeSelectedMethod('sms')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(selectedMethod==='sms')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/463816.png" alt=""></button>
+    <button v-on:click="changeSelectedMethod('email')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(selectedMethod==='email')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/email.png" alt=""></button>
+    <button v-on:click="changeSelectedMethod('google')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(selectedMethod==='google')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/google.png" alt=""></button>
+    <button v-on:click="changeSelectedMethod('linkedin')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(selectedMethod==='linkedin')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/145807.png" alt=""></button>
   </div>
 </template>
 
@@ -11,14 +11,14 @@
 export default {
   data(){
     return {
-      route: this.$route.name,
       selectedMethod: 'sms',  // options >> sms,email,google,linkedIn , default: sms
+
     }
   },
   methods: {
-    // which auth method (email-sms-google-linkedIn) is selected
-    getSelectedMethod(){
-
+    // change auth method (email-sms-google-linkedIn)
+    changeSelectedMethod(method){
+      this.selectedMethod = method
     },
   }
 }
