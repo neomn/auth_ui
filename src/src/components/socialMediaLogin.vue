9fac8e946bc6a93bfc73dde3de0da1aab0ba134c
neomn110 <neomn110@gmail.com>
2023-01-15 19:20:49 +0330

    refactor: change socialMedia items order

--- src/src/components/socialMediaLogin.vue
+++ src/src/components/socialMediaLogin.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-center sm:justify-center items-center bg-gray-800">
-    <button class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(route === 'login')? '':'grayscale'" ><img src="src/assets/icons/email.png" alt=""></button>
     <button class=" w-10 h-10 m-2 grayscale hover:grayscale-0 rounded-full"><img src="src/assets/icons/463816.png" alt=""></button>
+    <button class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(route === 'login')? '':'grayscale'" ><img src="src/assets/icons/email.png" alt=""></button>
     <button class=" w-10 h-10 m-2 grayscale hover:grayscale-0 rounded-full"><img src="src/assets/icons/google.png" alt=""></button>
     <button class=" w-10 h-10 m-2 grayscale hover:grayscale-0 rounded-full"><img src="src/assets/icons/145807.png" alt=""></button>
   </div>
@@ -12,7 +12,7 @@ export default {
   data(){
     return {
       route: this.$route.name,
-      selectedMethod: 'sms',  // optoins>> sms,email,google,linkedIn
+      selectedMethod: 'sms',  // options >> sms,email,google,linkedIn , default: sms
     }
   },
   methods: {
