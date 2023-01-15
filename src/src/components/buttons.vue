73e830d30d192a2fcc9af16ec83fe78cb7bb0725
neomn110 <neomn110@gmail.com>
2023-01-15 10:09:07 +0330

    style: change login background

--- src/src/components/buttons.vue
+++ src/src/components/buttons.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-around sm:justify-evenly items-center text-sm text-gray-400 overflow-hidden">
+  <div class="flex justify-around sm:justify-evenly items-center text-sm text-gray-400 bg-gray-700 overflow-hidden">
     <button class="basis-1/3 sm:basis-1/6 m-1 flex flex-col items-center p-1 ">
       <img src="src/assets/icons/login.png" class="w-6"/>
       Login
--- src/src/components/socialMediaLogin.vue
+++ src/src/components/socialMediaLogin.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-center sm:justify-center items-center">
+  <div class="flex justify-center sm:justify-center items-center bg-gray-700">
     <button class=" w-10 h-10 m-2 grayscale hover:grayscale-0 rounded-full"><img src="src/assets/icons/email.png" alt=""></button>
     <button class=" w-10 h-10 m-2 grayscale hover:grayscale-0 rounded-full"><img src="src/assets/icons/463816.png" alt=""></button>
     <button class=" w-10 h-10 m-2 grayscale hover:grayscale-0 rounded-full"><img src="src/assets/icons/google.png" alt=""></button>
