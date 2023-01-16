c4433553466d7b136bcaa5d97ac63b2a370c1163
neomn110 <neomn110@gmail.com>
2023-01-16 18:54:40 +0330

    fix: fix authMethods.vue icon names

--- src/src/components/authMethods.vue
+++ src/src/components/authMethods.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex justify-center sm:justify-center items-center bg-gray-800">
-    <button v-on:click="changeSelectedMethod('sms')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(selectedMethod==='sms')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/463816.png" alt=""></button>
+    <button v-on:click="changeSelectedMethod('sms')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(selectedMethod==='sms')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/sms.png" alt=""></button>
     <button v-on:click="changeSelectedMethod('email')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(selectedMethod==='email')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/email.png" alt=""></button>
     <button v-on:click="changeSelectedMethod('google')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(selectedMethod==='google')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/google.png" alt=""></button>
-    <button v-on:click="changeSelectedMethod('linkedin')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(selectedMethod==='linkedin')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/145807.png" alt=""></button>
+    <button v-on:click="changeSelectedMethod('linkedin')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(selectedMethod==='linkedin')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/linkedin.png" alt=""></button>
   </div>
 </template>
 
