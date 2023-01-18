702915f7ed5428dd2f3d85f500a57e0826888c14
neomn110 <neomn110@gmail.com>
2023-01-18 14:21:11 +0330

    refactor: rename method

--- src/src/components/authMethods.vue
+++ src/src/components/authMethods.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex justify-center sm:justify-center items-center bg-gray-800">
-    <button v-on:click="changeSelectedMethod('sms')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(authMethodCurrentLocation==='sms')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/sms.png" alt=""></button>
-    <button v-on:click="changeSelectedMethod('email')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(authMethodCurrentLocation==='email')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/email.png" alt=""></button>
-    <button v-on:click="changeSelectedMethod('google')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(authMethodCurrentLocation==='google')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/google.png" alt=""></button>
-    <button v-on:click="changeSelectedMethod('linkedin')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(authMethodCurrentLocation==='linkedin')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/linkedin.png" alt=""></button>
+    <button v-on:click="changeAuthMethod('sms')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(authMethodCurrentLocation==='sms')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/sms.png" alt=""></button>
+    <button v-on:click="changeAuthMethod('email')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(authMethodCurrentLocation==='email')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/email.png" alt=""></button>
+    <button v-on:click="changeAuthMethod('google')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(authMethodCurrentLocation==='google')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/google.png" alt=""></button>
+    <button v-on:click="changeAuthMethod('linkedin')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(authMethodCurrentLocation==='linkedin')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/linkedin.png" alt=""></button>
   </div>
 </template>
 
@@ -24,7 +24,7 @@ export default {
   },
   methods: {
     // change auth method (email-sms-google-linkedIn)
-    changeSelectedMethod(method){
+    changeAuthMethod(method){
       this.$store.commit('changeAuthMethod', method)
     },
   }
