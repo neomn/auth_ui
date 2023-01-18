2c6bc43e0928baeae5f7c1d8681c4956f50ebb13
neomn110 <neomn110@gmail.com>
2023-01-18 19:43:49 +0330

    Revert "add a test transition to auth method"
    This reverts commit 169dfdfef9c61de10ee38eb87284f5c1b515e0a1.

--- src/src/components/authMethods.vue
+++ src/src/components/authMethods.vue
@@ -3,7 +3,7 @@
     <button v-on:click="changeAuthMethod('sms')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(authMethod==='sms')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/sms.png"></button>
     <button v-on:click="changeAuthMethod('email')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(authMethod==='email')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/email.png"></button>
     <button v-on:click="changeAuthMethod('google')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(authMethod==='google')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/google.png"></button>
-    <button v-on:click="changeAuthMethod('linkedin')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full transition ease-linear duration-500" v-bind:class="(authMethod==='linkedin')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/linkedin.png"></button>
+    <button v-on:click="changeAuthMethod('linkedin')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(authMethod==='linkedin')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/linkedin.png"></button>
   </div>
 </template>
 
