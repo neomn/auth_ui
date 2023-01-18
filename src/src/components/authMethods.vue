e0dd2c2103c3f6f7e9c68f74070e3d9ed88fc036
neomn110 <neomn110@gmail.com>
2023-01-18 19:06:42 +0330

    refactor: remove alt from img tag

--- src/src/components/authMethods.vue
+++ src/src/components/authMethods.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex justify-center sm:justify-center items-center bg-gray-800">
-    <button v-on:click="changeAuthMethod('sms')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(authMethod==='sms')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/sms.png" alt=""></button>
-    <button v-on:click="changeAuthMethod('email')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(authMethod==='email')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/email.png" alt=""></button>
-    <button v-on:click="changeAuthMethod('google')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(authMethod==='google')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/google.png" alt=""></button>
-    <button v-on:click="changeAuthMethod('linkedin')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(authMethod==='linkedin')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/linkedin.png" alt=""></button>
+    <button v-on:click="changeAuthMethod('sms')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(authMethod==='sms')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/sms.png"></button>
+    <button v-on:click="changeAuthMethod('email')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(authMethod==='email')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/email.png"></button>
+    <button v-on:click="changeAuthMethod('google')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(authMethod==='google')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/google.png"></button>
+    <button v-on:click="changeAuthMethod('linkedin')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(authMethod==='linkedin')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/linkedin.png"></button>
   </div>
 </template>
 
@@ -16,5 +16,4 @@ export default {
     changeAuthMethod(method){ this.$store.dispatch('changeAuthMethod', method)},
   }
 }
-
 </script>
