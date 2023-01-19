f82c9371357ab4a9725963d4ec0f1e1f47d0393c
neomn110 <neomn110@gmail.com>
2023-01-19 21:05:36 +0330

    fix: fix overflow

--- src/src/components/FormInput.vue
+++ src/src/components/FormInput.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex justify-start items-center w-11/12 h-8 m-1 px-1 border border-gray-900 rounded-lg text-gray-300 bg-gray-600">
-    <img src="../assets/icons/email.png" class="w-4 h-4 grayscale">
+  <div class="flex justify-start items-center w-11/12 h-8 m-1  border border-gray-900 rounded-lg text-gray-300 bg-gray-600 overflow-hidden">
+    <img src="../assets/icons/email.png" class="w-4 h-4 mx-2 grayscale">
     <input id="email" type="email" placeholder="Email" class="px-2 bg-gray-600">
   </div>
 </template>
