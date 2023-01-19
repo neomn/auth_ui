540a0ee4c58e9659ece617f19e8c82d8fb418bb9
neomn110 <neomn110@gmail.com>
2023-01-19 21:07:06 +0330

    style: increase input width

--- src/src/components/FormInput.vue
+++ src/src/components/FormInput.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-start items-center w-11/12 h-8 m-1  border border-gray-900 rounded-lg text-gray-300 bg-gray-600 overflow-hidden">
     <img src="../assets/icons/email.png" class="w-4 h-4 mx-2 grayscale">
-    <input id="email" type="email" placeholder="Email" class="px-2 bg-gray-600">
+    <input id="email" type="email" placeholder="Email" class="w-full px-2 bg-gray-600">
   </div>
 </template>
 
