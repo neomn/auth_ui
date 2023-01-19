d0fe0c87aab3e574c31d472a6bb0896f2cb1e997
neomn110 <neomn110@gmail.com>
2023-01-19 15:56:29 +0330

    refactor: rename loginIputs.vue

--- src/src/components/root.vue
+++ src/src/components/root.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col sm:flex-row bg-gray-700">
     <WebsiteLogo class="basis-4/12 sm:basis-1/2 m-2 mb-1 sm:mb-2 "/>
     <div class="basis-8/12 sm:basis-1/2 m-2 mt-1 sm:mt-2 border-2 border-gray-900 rounded-2xl overflow-hidden ">
-      <loginInputs class="h-4/6"/>
+      <Inputs class="h-4/6"/>
       <AuthMethods class="h-1/6"/>
       <NavButtons class="h-1/6"/>
     </div>
@@ -12,13 +12,13 @@
 
 <script>
 import WebsiteLogo from "./websiteLogo.vue";
-import LoginInputs from "./loginInputs.vue";
+import Inputs from "./Inputs.vue";
 import AuthMethods from "./authMethods.vue";
 import RegisterInputs from "./registerInputs.vue";
 import NavButtons from "./navButtons.vue"
 
 export default {
-  components: {WebsiteLogo, AuthMethods, LoginInputs, RegisterInputs, NavButtons},
+  components: {WebsiteLogo, AuthMethods, Inputs, RegisterInputs, NavButtons},
   name: 'main',
 
   data() {
