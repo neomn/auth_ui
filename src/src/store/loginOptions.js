64eacf3c9e199756629063b72c026f008b9da836
neomn110 <neomn110@gmail.com>
2023-01-22 18:56:32 +0330

    refactor: write which buttons should be active for login using sms

--- src/src/store/loginOptions.js
+++ src/src/store/loginOptions.js
@@ -4,12 +4,15 @@ import {store} from "./store.js";
 function inputsForLoginUsingSms() {
     store.state.activeFormInputs.phone_number.display = true
     store.state.activeFormInputs.password.display = true
-
     store.state.activeFormInputs.email.display = false
     store.state.activeFormInputs.first_name.display = false
     store.state.activeFormInputs.last_name.display = false
     store.state.activeFormInputs.confirm_password.display = false
     store.state.activeFormInputs.one_time_token.display = false
+
+    store.state.activeButtons.login.display = true
+    store.state.activeButtons.register.display = false
+    store.state.activeButtons.recover.display = false
 }
 
 function inputsForLoginUsingEmail() {
