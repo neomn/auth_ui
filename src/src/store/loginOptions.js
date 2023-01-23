cbdf2149d5f28243bd679432111db75a92ddd4e6
neomn110 <neomn110@gmail.com>
2023-01-23 18:45:13 +0330

    fix: displaced function naming

--- src/src/store/loginOptions.js
+++ src/src/store/loginOptions.js
@@ -6,36 +6,40 @@ const inputsForLinkedin = {formInputs: [], buttons: []}
 
 function displayLoginInputs(state){
     if (state.authMethod === 'sms') {displaySmsInputs(state)}
-    if (state.authMethod === 'google') {displayEmailInputs(state)}
-    if (state.authMethod === 'email') {displayGoogleInputs(state)}
+    if (state.authMethod === 'email') {displayEmailInputs(state)}
+    if (state.authMethod === 'google') {displayGoogleInputs(state)}
     if (state.authMethod === 'linkedin') {displayLinkedinInputs(state)}
 }
 
 function displaySmsInputs(state){
+    console.log(state.authMethod)
     if (inputsForSms.formInputs.length > 0){
         Object.entries(state.activeFormInputs).forEach((formInput) => {
             inputsForSms.formInputs.includes(formInput[0]) ? formInput[1].display = true : formInput[1].display = false
         })
-    } else {
-        Object.entries(state.activeFormInputs).forEach((formInput) => {
-            formInput[1].display = false
-        })
-    }
+    } else Object.entries(state.activeFormInputs).forEach((formInput) => { formInput[1].display = false })
+
     if (inputsForSms.buttons.length > 0){
         Object.entries(state.activeButtons).forEach((button) => {
             inputsForSms.buttons.includes(button[0]) ? button[1].display = true : button[1].display = false
         })
-    }else {
-        Object.entries(state.activeButtons).forEach((button) => {
-            button[1].display = false
-        })
-    }
-    console.log(Object.values(state.activeFormInputs))
+    } else  Object.entries(state.activeButtons).forEach((button) => {button[1].display = false })
 }
 
 
 function displayEmailInputs(state) {
+    console.log(state.authMethod)
+    if (inputsForEmail.formInputs.length > 0){
+        Object.entries(state.activeFormInputs).forEach((formInput) => {
+            inputsForEmail.formInputs.includes(formInput[0]) ? formInput[1].display = true : formInput[1].display = false
+        })
+    } else Object.entries(state.activeFormInputs).forEach((formInput) => { formInput[1].display = false })
 
+    if (inputsForEmail.buttons.length > 0){
+        Object.entries(state.activeButtons).forEach((button) => {
+            inputsForEmail.buttons.includes(button[0]) ? button[1].display = true : button[1].display = false
+        })
+    } else  Object.entries(state.activeButtons).forEach((button) => {button[1].display = false })
 }
 
 
