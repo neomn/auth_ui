2c883d14ddc480f32c869dfba3645074e659b878
neomn110 <neomn110@gmail.com>
2023-01-23 18:33:43 +0330

    refactor: hadle an edge case for empty array in inputs

--- src/src/store/loginOptions.js
+++ src/src/store/loginOptions.js
@@ -5,6 +5,13 @@ const inputsForGoogle = {formInputs: [], buttons: []}
 const inputsForLinkedin = {formInputs: [], buttons: []}
 
 function displayLoginInputs(state){
+    if (state.authMethod === 'sms') {displaySmsInputs(state)}
+    if (state.authMethod === 'google') {displayEmailInputs(state)}
+    if (state.authMethod === 'email') {displayGoogleInputs(state)}
+    if (state.authMethod === 'linkedin') {displayLinkedinInputs(state)}
+}
+
+function displaySmsInputs(state){
     if (inputsForSms.formInputs.length > 0){
         Object.entries(state.activeFormInputs).forEach((formInput) => {
             inputsForSms.formInputs.includes(formInput[0]) ? formInput[1].display = true : formInput[1].display = false
@@ -14,7 +21,6 @@ function displayLoginInputs(state){
             formInput[1].display = false
         })
     }
-
     if (inputsForSms.buttons.length > 0){
         Object.entries(state.activeButtons).forEach((button) => {
             inputsForSms.buttons.includes(button[0]) ? button[1].display = true : button[1].display = false
@@ -24,11 +30,22 @@ function displayLoginInputs(state){
             button[1].display = false
         })
     }
-
     console.log(Object.values(state.activeFormInputs))
 }
 
 
+function displayEmailInputs(state) {
 
+}
+
+
+function displayGoogleInputs(state) {
+
+}
+
+
+function displayLinkedinInputs(state) {
+
+}
 
 export {displayLoginInputs}
