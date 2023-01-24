0046241e25674e13e3307c343bebe2f3e59d6283
neomn110 <neomn110@gmail.com>
2023-01-24 17:41:27 +0330

    feat: use auto-animate plugin in app

--- src/src/main.js
+++ src/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import { autoAnimatePlugin } from "@formkit/auto-animate/vue";
 import { store } from "./store/store.js"
 import { routes } from './routes'
 import App from './App.vue'
@@ -13,4 +14,5 @@ const router = createRouter({
 const app = createApp(App)
 app.use(router)
 app.use(store)
+app.use(autoAnimatePlugin)
 app.mount('#app')
