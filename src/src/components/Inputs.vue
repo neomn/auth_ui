0c671594156f9743702310699da157ea49cebbab
neomn110 <neomn110@gmail.com>
2023-01-24 19:17:34 +0330

    refactor: auto animate works now

--- src/src/components/Inputs.vue
+++ src/src/components/Inputs.vue
@@ -1,6 +1,6 @@
 <template>
-  <div v-auto-animate class="flex flex-col content-end text-white bg-gray-800 overflow-hidden ">
-    <div  id="inputs" class="flex flex-col justify-start items-center basis-4/6 w-full mt-2 sm:pt-10 overflow-x-hidden overflow-y-scroll">
+  <div class="flex flex-col content-end text-white bg-gray-800 overflow-hidden ">
+    <div v-auto-animate id="inputs" class="flex flex-col justify-start items-center basis-4/6 w-full mt-2 sm:pt-10 overflow-x-hidden overflow-y-scroll">
       <FormInput id="phone_number" :placeholder="Object.keys(activeFormInputs)[0]" v-if="activeFormInputs.phone_number.display" class=""/>
       <FormInput id="email" :placeholder="Object.keys(activeFormInputs)[1]" v-if="activeFormInputs.email.display" class=""/>
       <FormInput id="first_name" :placeholder="Object.keys(activeFormInputs)[2]" v-if="activeFormInputs.first_name.display" class=""/>
@@ -9,7 +9,7 @@
       <FormInput id="confirm_password" :placeholder="Object.keys(activeFormInputs)[5]" v-if="activeFormInputs.confirm_password.display" class=""/>
       <FormInput id="one_time_token" :placeholder="Object.keys(activeFormInputs)[6]" v-if="activeFormInputs.one_time_token.display" class=""/>
     </div>
-    <div id="submitButton" class=" basis-2/6 flex justify-around items-center w-full">
+    <div v-auto-animate id="submitButton" class=" basis-2/6 flex justify-around items-center w-full">
       <Button id="login" :text="Object.keys(activeButtons)[0]" v-if="activeButtons.login.display"/>
       <Button id="register" :text="Object.keys(activeButtons)[1]" v-if="activeButtons.register.display"/>
       <Button id="recover" :text="Object.keys(activeButtons)[2]" v-if="activeButtons.recover.display"/>
