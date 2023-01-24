257920d21be6b3005c20895742d623dd9ca2313e
neomn110 <neomn110@gmail.com>
2023-01-24 19:22:31 +0330

    refactor: auto animate authMethods.vue

--- src/src/components/AuthMethods.vue
+++ src/src/components/AuthMethods.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-center sm:justify-center items-center bg-gray-800">
+  <div v-auto-animate class="flex justify-center sm:justify-center items-center bg-gray-800">
     <button v-on:click="onClickAuthMethod('sms')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(authMethod==='sms')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/sms.png"></button>
     <button v-on:click="onClickAuthMethod('email')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(authMethod==='email')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/email.png"></button>
     <button v-on:click="onClickAuthMethod('google')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(authMethod==='google')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/google.png"></button>
