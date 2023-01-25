61bb4854f2ea6c3f3796189e1bd1f6be2e1c5f1c
neomn110 <neomn110@gmail.com>
2023-01-25 09:08:42 +0330

    feat: show desired auth methods for login route

--- src/src/components/AuthMethods.vue
+++ src/src/components/AuthMethods.vue
@@ -1,9 +1,9 @@
 <template>
   <div v-auto-animate class="flex justify-center sm:justify-center items-center bg-gray-800">
-    <button v-on:click="onClickAuthMethod('sms')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(currentAuthMethod==='sms')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/sms.png"></button>
-    <button v-on:click="onClickAuthMethod('email')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(currentAuthMethod==='email')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/email.png"></button>
-    <button v-on:click="onClickAuthMethod('google')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(currentAuthMethod==='google')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/google.png"></button>
-    <button v-on:click="onClickAuthMethod('linkedin')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(currentAuthMethod==='linkedin')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/linkedin.png"></button>
+    <button v-if="authMethods.sms.display" v-on:click="onClickAuthMethod('sms')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(currentAuthMethod==='sms')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/sms.png"></button>
+    <button v-if="authMethods.email.display" v-on:click="onClickAuthMethod('email')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(currentAuthMethod==='email')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/email.png"></button>
+    <button v-if="authMethods.google.display" v-on:click="onClickAuthMethod('google')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(currentAuthMethod==='google')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/google.png"></button>
+    <button v-if="authMethods.linkedin.display" v-on:click="onClickAuthMethod('linkedin')" class=" w-10 h-10 m-2 hover:grayscale-0 rounded-full" v-bind:class="(currentAuthMethod==='linkedin')? 'grayscale-0':'grayscale'"><img src="src/assets/icons/linkedin.png"></button>
   </div>
 </template>
 
--- src/src/store/store.js
+++ src/src/store/store.js
@@ -50,6 +50,9 @@ export const store = new Vuex.Store({
         currentAuthMethod: state => {
           return state.currentAuthMethod
         },
+        authMethods: (state) => {
+            return state.authMethods
+        }
     },
 
     mutations: {
