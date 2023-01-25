aeda86fd4d01354882553a07e4ad027141283b37
neomn110 <neomn110@gmail.com>
2023-01-25 10:32:43 +0330

    feat: create displayManager.js

---
+++ src/src/store/displayManager.js
@@ -0,0 +1,19 @@
+
+function setWhichFormInputsShouldBeDisplayed(state, inputsToDisplay){
+
+}
+
+function setWhichAuthMethodsShouldBeDisplayed(state, AuthMethodsToDisplay){
+
+}
+
+function setWhichButtonsShouldBeDisplayed(state, buttonsToDisplay){
+
+}
+
+export {
+    setWhichFormInputsShouldBeDisplayed,
+    setWhichAuthMethodsShouldBeDisplayed,
+    setWhichButtonsShouldBeDisplayed
+}
+
