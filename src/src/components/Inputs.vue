ca1cc8f669dfef5b246380c1c53eead5330aefe6
neomn110 <neomn110@gmail.com>
2023-01-29 07:24:11 +0330

    refactor: display forminput placeholder

--- src/src/components/Inputs.vue
+++ src/src/components/Inputs.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="flex flex-col content-end text-white bg-gray-800 overflow-hidden ">
     <div v-auto-animate id="inputs" class="flex flex-col justify-start items-center basis-4/6 w-full mt-2 sm:pt-10 overflow-x-hidden overflow-y-scroll">
-      <FormInput id="phone_number" :placeholder="Object.keys(formInputs)[0]" v-if="formInputs.phone_number.display" class=""/>
-      <FormInput id="email" :placeholder="Object.keys(formInputs)[1]" v-if="formInputs.email.display" class=""/>
-      <FormInput id="first_name" :placeholder="Object.keys(formInputs)[2]" v-if="formInputs.first_name.display" class=""/>
-      <FormInput id="last_name" :placeholder="Object.keys(formInputs)[3]" v-if="formInputs.last_name.display" class=""/>
-      <FormInput id="password" :placeholder="Object.keys(formInputs)[4]" v-if="formInputs.password.display" class=""/>
-      <FormInput id="confirm_password" :placeholder="Object.keys(formInputs)[5]" v-if="formInputs.confirm_password.display" class=""/>
-      <FormInput id="one_time_token" :placeholder="Object.keys(formInputs)[6]" v-if="formInputs.one_time_token.display" class=""/>
+      <FormInput id="phone_number" :placeholder="formInputs.phone_number.placeHolder" v-if="formInputs.phone_number.display" class=""/>
+      <FormInput id="email" :placeholder="formInputs.email.placeHolder" v-if="formInputs.email.display" class=""/>
+      <FormInput id="first_name" :placeholder="formInputs.first_name.placeHolder" v-if="formInputs.first_name.display" class=""/>
+      <FormInput id="last_name" :placeholder="formInputs.last_name.placeHolder" v-if="formInputs.last_name.display" class=""/>
+      <FormInput id="password" :placeholder="formInputs.password.placeHolder" v-if="formInputs.password.display" class=""/>
+      <FormInput id="confirm_password" :placeholder="formInputs.confirm_password.placeHolder" v-if="formInputs.confirm_password.display" class=""/>
+      <FormInput id="one_time_token" :placeholder="formInputs.one_time_token.placeHolder" v-if="formInputs.one_time_token.display" class=""/>
     </div>
     <div v-auto-animate id="submitButton" class=" basis-2/6 flex justify-around items-center w-full">
       <Button id="login" :text="Object.keys(buttons)[0]" v-if="buttons.login.display"/>
