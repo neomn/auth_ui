86a23f643e47d9f95a80141c8780e3a8773a1aa9
neomn110 <neomn110@gmail.com>
2023-01-30 08:09:53 +0330

    feat: display icon for password dynamically

--- src/src/components/Inputs.vue
+++ src/src/components/Inputs.vue
@@ -5,7 +5,7 @@
       <FormInput id="email" :placeholder="formInputs.email.placeHolder" v-if="formInputs.email.display" class=""/>
       <FormInput id="first_name" :placeholder="formInputs.first_name.placeHolder" v-if="formInputs.first_name.display" class=""/>
       <FormInput id="last_name" :placeholder="formInputs.last_name.placeHolder" v-if="formInputs.last_name.display" class=""/>
-      <FormInput id="password"  :placeholder="formInputs.password.placeHolder" v-if="formInputs.password.display" class=""/>
+      <FormInput id="password" :icon="icons.formInputs.password" :placeholder="formInputs.password.placeHolder" v-if="formInputs.password.display" class=""/>
       <FormInput id="confirm_password" :placeholder="formInputs.confirm_password.placeHolder" v-if="formInputs.confirm_password.display" class=""/>
       <FormInput id="one_time_token" :placeholder="formInputs.one_time_token.placeHolder" v-if="formInputs.one_time_token.display" class=""/>
     </div>
