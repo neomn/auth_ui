3ac36f7453d83304bd5b79a6b0aefb9fc5e62b50
neomn110 <neomn110@gmail.com>
2023-02-04 10:19:02 +0330

    feat: update formInput value inside store using v-model

--- src/src/components/FormInput.vue
+++ src/src/components/FormInput.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-start items-center w-11/12 h-8 m-1  border border-gray-900 rounded-lg text-gray-300 bg-gray-600 overflow-hidden">
     <img :src="icon" class="w-4 h-4 mx-2">
-    <input v-model="value" @input="syncValueWithStore(value)" :type="type" :placeholder="placeholder" class="w-full px-2 bg-gray-600">
+    <input v-model="formInputValue" :type="type" :placeholder="placeholder" class="w-full px-2 bg-gray-600">
   </div>
   <label>{{value}}</label>
 </template>
@@ -10,13 +10,11 @@
 export default {
   name: "FormInput",
   props: ['storeId','icon', 'type', 'placeholder'],
-  data() {
-    return {
-      value:''
-    }
-  },
-  methods: {
-    syncValueWithStore(){ this.$store.dispatch('updateFormInputValue', {'storeId': this.storeId, 'value': this.value}) },
+  computed: {
+    formInputValue: {
+      get() { return this.$store.getters.formInputs[this.storeId].value },
+      set(value) {this.$store.dispatch('updateFormInputValue', {storeId: this.storeId, value: value})},
+    },
   },
 }
 </script>
