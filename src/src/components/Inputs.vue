d0c00bfc6acf97139c61537c451ad93e67fc801b
neomn110 <neomn110@gmail.com>
2023-02-04 07:00:42 +0330

    refactor: trying to bind form inputs with their values in vuex store

--- src/src/components/Inputs.vue
+++ src/src/components/Inputs.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col content-end text-white bg-gray-800 overflow-hidden ">
     <div v-auto-animate id="inputs" class="flex flex-col justify-start items-center basis-4/6 w-full mt-2 sm:pt-10 overflow-x-hidden overflow-y-scroll">
-      <FormInput id="phone_number" :icon="icons.formInputs.phone_number" :type="formInputs.phone_number.type" :placeholder="formInputs.phone_number.placeHolder" v-if="formInputs.phone_number.display" class=""/>
+      <FormInput id="phone_number" v-model="phone_number" :icon="icons.formInputs.phone_number" :type="formInputs.phone_number.type" :placeholder="formInputs.phone_number.placeHolder" v-if="formInputs.phone_number.display" class=""/>
       <FormInput id="email" :icon="icons.formInputs.email" :placeholder="formInputs.email.placeHolder" v-if="formInputs.email.display" class=""/>
       <FormInput id="first_name" :icon="icons.formInputs.first_name" :placeholder="formInputs.first_name.placeHolder" v-if="formInputs.first_name.display" class=""/>
       <FormInput id="last_name" :icon="icons.formInputs.last_name" :placeholder="formInputs.last_name.placeHolder" v-if="formInputs.last_name.display" class=""/>
@@ -14,6 +14,7 @@
       <Button id="register" :text="buttons.register.placeHolder" v-if="buttons.register.display"/>
       <Button id="recover" :text="buttons.recover.placeHolder" v-if="buttons.recover.display"/>
       <Button id="check" :text="buttons.check.placeholder" v-if="buttons.check.display"/>
+<!--      <p>{{ phone_number }}</p>-->
     </div>
   </div>
 </template>
@@ -29,7 +30,9 @@ export default{
   computed: {
     formInputs(){return this.$store.getters.formInputs},
     buttons(){return this.$store.getters.buttons},
-    icons(){return this.$store.getters.icons}
+    icons(){return this.$store.getters.icons},
+    phone_number(){return this.$store.state.getters.formInputs.phone_number.value},
+
   },
   mounted() {
     this.$store.dispatch('updateInputComponent', this.$route.name)
