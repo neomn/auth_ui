77665c039d55046acb9fab8d6dda866917069307
neomn110 <neomn110@gmail.com>
2023-02-07 12:19:31 +0330

    feat: write changeInputRingColor function

--- src/src/components/FormInput.vue
+++ src/src/components/FormInput.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-start items-center w-11/12 h-8 m-1  border border-gray-900 rounded-lg text-gray-300 bg-gray-600 overflow-hidden">
     <img :src="icon" class="w-4 h-4 mx-2">
-    <input v-model="inputValue" @input="inputValueHasChanged(inputId, inputValue)" :type="type" :placeholder="placeholder" class="w-full px-2 bg-gray-600">
+    <input v-model="inputValue" @input="inputValueHasChanged(inputId, inputValue)" ref="input" :type="type" :placeholder="placeholder" class="w-full px-2 bg-gray-600">
   </div>
 </template>
 
@@ -16,7 +16,7 @@ export default {
     },
     inputValidProperty: {
       get(){return this.$store.getters.formInputs[this.inputId].valid},
-      set(value){this.$store.dispatch('updateFormInputValidProperty',{ inputId: this.inputId, valid: value})},
+      set(value){this.$store.dispatch('updateFormInputValidateAndDirtyProperties',{ inputId: this.inputId, valid: value})},
     },
   },
   methods: {
@@ -25,15 +25,34 @@ export default {
       this.updateInputValidateProperty(inputId)
       this.changeInputRingColor(inputId)
     },
+
+
     touchInput(inputId){
 
     },
+
+
     updateInputValidateProperty(inputId){
 
     },
-    changeInputRingColor(inputId){
 
-    }
+
+    changeInputRingColor(inputId){
+      const validColor = 'border-lime-400'
+      const invalidColor = 'border-red-300'
+
+      if ( this.inputValidProperty ){
+        this.$refs.input.classList.remove(invalidColor)
+        this.$refs.input.classList.add(validColor)
+      }
+
+      if ( !this.inputValidProperty ){
+        this.$refs.input.classList.remove(validColor)
+        this.$refs.input.classList.add(invalidColor)
+      }
+    },
+
+
   }
 }
 </script>
