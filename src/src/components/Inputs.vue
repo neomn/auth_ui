48fc55040745baa0f247a2e958c1199129058e4f
neomn110 <neomn110@gmail.com>
2023-02-08 08:05:05 +0330

    feat: handle no input is focused

--- src/src/components/Inputs.vue
+++ src/src/components/Inputs.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="flex flex-col content-end text-white bg-gray-800 overflow-hidden ">
     <div v-auto-animate id="inputs" class="flex flex-col justify-start items-center basis-4/6 w-full mt-2 sm:pt-10 overflow-x-hidden overflow-y-scroll">
-      <FormInput inputId="phone_number" @inputFocused="updateCurrentlyFocusedInput('phone_number')" @inputValueHasChanged="inputValueHasChanged('phone_number')" :icon="icons.formInputs.phone_number" :type="formInputs.phone_number.type" :placeholder="formInputs.phone_number.placeHolder" v-if="formInputs.phone_number.display" class=""/>
-      <FormInput inputId="email" @inputFocused="updateCurrentlyFocusedInput('email')" @inputValueHasChanged="inputValueHasChanged('email')" :icon="icons.formInputs.email" :placeholder="formInputs.email.placeHolder" v-if="formInputs.email.display" class=""/>
-      <FormInput inputId="first_name" @inputFocused="updateCurrentlyFocusedInput('first_name')"  @inputValueHasChanged="inputValueHasChanged('first_name')" :icon="icons.formInputs.first_name" :placeholder="formInputs.first_name.placeHolder" v-if="formInputs.first_name.display" class=""/>
-      <FormInput inputId="last_name" @inputFocused="updateCurrentlyFocusedInput('last_name')"  @inputValueHasChanged="inputValueHasChanged('last_name')" :icon="icons.formInputs.last_name" :placeholder="formInputs.last_name.placeHolder" v-if="formInputs.last_name.display" class=""/>
-      <FormInput inputId="password"  @inputFocused="updateCurrentlyFocusedInput('password')"  @inputValueHasChanged="inputValueHasChanged('password')"  :icon="icons.formInputs.password" :placeholder="formInputs.password.placeHolder" v-if="formInputs.password.display" class=""/>
-      <FormInput inputId="confirm_password"  @inputFocused="updateCurrentlyFocusedInput('confirm_password')"  @inputValueHasChanged="inputValueHasChanged('confirm_password')" :icon="icons.formInputs.confirm_password" :placeholder="formInputs.confirm_password.placeHolder" v-if="formInputs.confirm_password.display" class=""/>
-      <FormInput inputId="one_time_token"  @inputFocused="updateCurrentlyFocusedInput('one_time_token')"  @inputValueHasChanged="inputValueHasChanged('one_time_token')" :icon="icons.formInputs.one_time_token" :placeholder="formInputs.one_time_token.placeHolder" v-if="formInputs.one_time_token.display" class=""/>
+      <FormInput inputId="phone_number" @inputFocused="updateCurrentlyFocusedInput('phone_number')" @inputFocusedOut="updateCurrentlyFocusedInput('')" @inputValueHasChanged="inputValueHasChanged('phone_number')" :icon="icons.formInputs.phone_number" :type="formInputs.phone_number.type" :placeholder="formInputs.phone_number.placeHolder" v-if="formInputs.phone_number.display" class=""/>
+      <FormInput inputId="email" @inputFocused="updateCurrentlyFocusedInput('email')" @inputFocusedOut="updateCurrentlyFocusedInput('')"  @inputValueHasChanged="inputValueHasChanged('email')" :icon="icons.formInputs.email" :placeholder="formInputs.email.placeHolder" v-if="formInputs.email.display" class=""/>
+      <FormInput inputId="first_name" @inputFocused="updateCurrentlyFocusedInput('first_name')" @inputFocusedOut="updateCurrentlyFocusedInput('')"   @inputValueHasChanged="inputValueHasChanged('first_name')" :icon="icons.formInputs.first_name" :placeholder="formInputs.first_name.placeHolder" v-if="formInputs.first_name.display" class=""/>
+      <FormInput inputId="last_name" @inputFocused="updateCurrentlyFocusedInput('last_name')" @inputFocusedOut="updateCurrentlyFocusedInput('')"   @inputValueHasChanged="inputValueHasChanged('last_name')" :icon="icons.formInputs.last_name" :placeholder="formInputs.last_name.placeHolder" v-if="formInputs.last_name.display" class=""/>
+      <FormInput inputId="password"  @inputFocused="updateCurrentlyFocusedInput('password')" @inputFocusedOut="updateCurrentlyFocusedInput('')"   @inputValueHasChanged="inputValueHasChanged('password')"  :icon="icons.formInputs.password" :placeholder="formInputs.password.placeHolder" v-if="formInputs.password.display" class=""/>
+      <FormInput inputId="confirm_password"  @inputFocused="updateCurrentlyFocusedInput('confirm_password')" @inputFocusedOut="updateCurrentlyFocusedInput('')"   @inputValueHasChanged="inputValueHasChanged('confirm_password')" :icon="icons.formInputs.confirm_password" :placeholder="formInputs.confirm_password.placeHolder" v-if="formInputs.confirm_password.display" class=""/>
+      <FormInput inputId="one_time_token"  @inputFocused="updateCurrentlyFocusedInput('one_time_token')" @inputFocusedOut="updateCurrentlyFocusedInput('')"   @inputValueHasChanged="inputValueHasChanged('one_time_token')" :icon="icons.formInputs.one_time_token" :placeholder="formInputs.one_time_token.placeHolder" v-if="formInputs.one_time_token.display" class=""/>
     </div>
     <div v-auto-animate id="submitButton" class=" basis-2/6 flex justify-around items-center w-full">
       <Button id="login" @click="login" :text="buttons.login.placeHolder" v-if="buttons.login.display"/>
@@ -91,6 +91,7 @@ export default{
     },
     updateCurrentlyFocusedInput(input){
       this.$store.dispatch("updateCurrentlyFocusedInput",input)
+      console.log(input)
     },
   },
   validations(){
