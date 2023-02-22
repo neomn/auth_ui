089836c07dd8c588afe2187b039e2e7d55d3ec98
neomn110 <neomn110@gmail.com>
2023-02-22 18:14:08 +0330

    style: modify auth methods component padding

--- src/src/components/Root.vue
+++ src/src/components/Root.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex flex-col sm:flex-row bg-gray-700">
     <WebsiteLogo class="basis-4/12 sm:basis-1/2 m-2 mb-1 sm:mb-2 "/>
-    <div class="basis-8/12 sm:basis-1/2 m-2 mt-1 sm:mt-2 border-2 border-gray-900 rounded-2xl overflow-hidden ">
-      <AuthMethods class="h-1/6"/>
+    <div class="basis-8/12 flex flex-col justify-around sm:basis-1/2 m-2 mt-1 sm:mt-2 border-2 border-gray-900 rounded-2xl overflow-hidden ">
+      <AuthMethods class="h-1/6 pt-16 pb-10"/>
       <Inputs class="h-4/6"/>
       <NavButtons class="h-1/6"/>
     </div>
