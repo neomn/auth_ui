6ab669513fb7d27b59da9f824303cf5b91405800
neomn110 <neomn110@gmail.com>
2023-02-22 16:21:23 +0330

    style: move auth methods to the top of the inputs component

--- src/src/components/Root.vue
+++ src/src/components/Root.vue
@@ -2,8 +2,8 @@
   <div class="flex flex-col sm:flex-row bg-gray-700">
     <WebsiteLogo class="basis-4/12 sm:basis-1/2 m-2 mb-1 sm:mb-2 "/>
     <div class="basis-8/12 sm:basis-1/2 m-2 mt-1 sm:mt-2 border-2 border-gray-900 rounded-2xl overflow-hidden ">
-      <Inputs class="h-4/6"/>
       <AuthMethods class="h-1/6"/>
+      <Inputs class="h-4/6"/>
       <NavButtons class="h-1/6"/>
     </div>
   </div>
