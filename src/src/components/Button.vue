db7222a71b3fe4c7d7f851c3d203277c913a029d
neomn110 <neomn110@gmail.com>
2023-02-22 21:06:08 +0330

    feat: bind action button border color to the allInputsAreValidProperty status'

--- src/src/components/Button.vue
+++ src/src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-center items-center w-24 h-8 rounded border border-gray-600" >
+  <div class="flex justify-center items-center w-24 h-8 rounded border" :class="(allInputsAreValid) ? 'border-lime-300':'border-gray-600' ">
     {{ text }}
   </div>
 </template>
@@ -8,5 +8,8 @@
 export default {
   name: "Button",
   props: ['text'],
+  computed: {
+    allInputsAreValid(){return this.$store.getters.allInputsAreValid },
+  }
 }
 </script>
