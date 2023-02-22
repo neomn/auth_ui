989d6db4ea7529e46574c1e1c67edbc9b43683ef
neomn110 <neomn110@gmail.com>
2023-02-22 20:46:19 +0330

    style: set button border to gray

--- src/src/components/Button.vue
+++ src/src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-center items-center w-24 h-8 rounded border border-lime-300" >
+  <div class="flex justify-center items-center w-24 h-8 rounded border border-gray-600" >
     {{ text }}
   </div>
 </template>
