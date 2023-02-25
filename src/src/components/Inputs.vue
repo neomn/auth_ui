d45dddde9304240a61d2882fe93dfa7eae378c64
neomn110 <neomn110@gmail.com>
2023-02-25 16:32:31 +0330

    refactor: remove unnecessary commented codes

--- src/src/components/Inputs.vue
+++ src/src/components/Inputs.vue
@@ -118,55 +118,7 @@ export default{
 
   },
 
-
-  validations(){
-
-    return validationRules
-
-    // return {
-    //   phone_number: {
-    //     $autoDirty:true,
-    //     required,
-    //     numeric,
-    //     minLength: minLength(11),
-    //     maxLength: maxLength(11),
-    //   },
-    //   email: {
-    //     $autoDirty:true,
-    //     required,
-    //     email,
-    //     maxLength: maxLength(64),
-    //   },
-    //   first_name: {
-    //     $autoDirty:true,
-    //     required,
-    //     alpha,
-    //     minLength: minLength(32),
-    //     maxLength: maxLength(32),
-    //   },
-    //   last_name: {
-    //     $autoDirty:true,
-    //     required,
-    //     alpha,
-    //     minLength: minLength(32),
-    //     maxLength: maxLength(32),
-    //   },
-    //   password: {
-    //     $autoDirty:true,
-    //     required,
-    //     minLength: minLength(8),
-    //     maxLength: maxLength(32),
-    //   },
-    //   one_time_token: {
-    //     $autoDirty:true,
-    //     required,
-    //     numeric,
-    //     minLength: minLength(6),
-    //     maxLength: maxLength(6),
-    //   },
-    // }
-
-  },
+  validations(){ return validationRules },
 
 }
 
