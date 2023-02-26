59620e5245a24abadd6f530255354c7c13d5be39
neomn110 <neomn110@gmail.com>
2023-02-26 21:30:39 +0330

    Revert "feat: create v$ mutation"
    This reverts commit 416690e723383e978b1bf15c544be14d35f3eaad.

--- src/src/store/store.js
+++ src/src/store/store.js
@@ -69,7 +69,6 @@ export const store = new Vuex.Store({
     },
 
     mutations: {
-        updateV$Object: (state, v$) => {state.v$ = v$},
         changeTheme: (state, payload) => { state.theme = payload },
         changeCurrentAuthMethod: (state, payload) => { state.currentAuthMethod = payload },
         changeSelectedNavButton: (state, payload) => { state.selectedNavButton = payload },
