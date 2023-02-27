577802354defdf6377cb2881cc8efc7cd1a725a9
neomn110 <neomn110@gmail.com>
2023-02-27 17:36:36 +0330

    feat: generate 30 keyframes for ping animation using chat-gpt:)

--- src/tailwind.config.cjs
+++ src/tailwind.config.cjs
@@ -12,6 +12,18 @@ module.exports = {
         animationDelay: '1s',
         animationIterationCount: '3',
       },
+      keyframes: {
+        'ping': {
+          //this piece of code is generated using chat-gpt :) , it is fo fun
+          '0%, 100%': { transform: 'scale(1)' },
+          // Generate 28 additional keyframes with scale values between 1 and 1.5
+          ...Array.from({ length: 28 }).reduce((keyframes, _, i) => {
+            const scale = 1 + (0.5 * (i + 1)) / 30
+            keyframes[`${(i + 1) * 100 / 30}%`] = { transform: `scale(${scale})` }
+            return keyframes
+          }, {}),
+        },
+      }
     },
   },
 }
