3fd6dc6d09ebb1572cbc81816fb689bd5fc2aab2
neomn110 <neomn110@gmail.com>
2023-03-01 19:24:01 +0330

    feat: change border color when an input valid property value changes

--- src/src/components/FormInput.vue
+++ src/src/components/FormInput.vue
@@ -1,7 +1,13 @@
 <template>
-  <div ref="input" class="flex justify-start items-center w-11/12 h-8 m-1  border rounded-lg
-        text-gray-300 bg-gray-600 overflow-hidden"
-        :class="(inputIsFocused )? 'border-gray-300':'border-gray-900'" >
+  <div ref="input" class="flex justify-start items-center w-11/12 h-8 m-1  border border-gray-900 rounded-lg text-gray-300
+        bg-gray-600 overflow-hidden"
+        :class="`${ inputIsFocused && inputValue.toString().length<=0 ? 'border-gray-300' : ''}
+                 ${ inputIsFocused && inputValue.toString().length>0 && !inputIsValid  ? 'border-red-500' : ''}
+                 ${ inputIsFocused && inputValue.toString().length>0 && inputIsValid ? 'border-lime-500' : ''}
+                 ${ inputIsValid ? 'border-lime-500' : ''}  `">
+
+  <!--        {{ inputIsFocused && !inputIsValid ? 'border-red-500':'' }}-->
+<!--        {{ inputIsFocused && inputIsValid  ? 'border-lime-400':'' }} ">-->
     <img :src="icon" class="w-4 h-4 mx-2">
     <input v-model="inputValue" @focus="inputFocused" @focusout="inputFocusedOut" @input="inputValueHasChanged(inputId, inputValue)" :type="type" :placeholder="placeholder" class="w-full px-2 bg-gray-600 focus:outline-none">
   </div>
@@ -26,16 +32,24 @@ export default {
   methods: {
     inputValueHasChanged(inputId){
       this.$emit('inputValueHasChanged')
-      console.log(this.inputIsValid)
+      this.printStatus()
     },
     inputFocused(){
       this.inputIsFocused = true
       this.$emit('inputFocused')
+      this.printStatus()
     },
     inputFocusedOut(){
       this.inputIsFocused = false
       this.$emit('inputFocusedOut')
+      this.printStatus()
+
     },
+    printStatus(){
+      // console.log(this.inputId+ ' focus status > '+ this.inputIsFocused+ "\n")
+      // console.log(this.inputId+ ' length > '+ this.inputValue.toString().length+ "\n")
+      // console.log(this.inputId+ ' validation status > '+ this.inputIsValid+ "\n\n")
+    }
   }
 }
 </script>
