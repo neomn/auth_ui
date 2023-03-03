509634df03f32ad6a53bc5c197dded3bbd266a7d
neomn110 <neomn110@gmail.com>
2023-03-03 21:31:17 +0330

    feat: add maxLength rule message

--- src/src/store/validationMessages.js
+++ src/src/store/validationMessages.js
@@ -1,7 +1,11 @@
-
+import {validationRules} from "./validationRules.js";
 export const validationMessages = {
     required: 'is required',
     alpha: 'should be alphabetic',
     email: 'should be valid email address',
     numeric: 'should be numeric',
+    maxLength: {
+        numeric: 'should be :max digits max',
+        alpha: 'should be :max character max',
+    },
 }
