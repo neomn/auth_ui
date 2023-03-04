d179edf32c9198fd800ada3cb90b2da0f0e19cc9
neomn110 <neomn110@gmail.com>
2023-03-04 20:57:34 +0330

    feat: init pending validation rules

--- src/src/validation/updateValidAndInvalidValidationRulesInTheStore.js
+++ src/src/validation/updateValidAndInvalidValidationRulesInTheStore.js
@@ -1,8 +1,11 @@
 import {store} from "../store/store.js";
+import {validationRules} from "./validationRules.js";
 
 export const sync = {
     init(){
-
+        Object.values(store.getters.formInputs).forEach(input => {
+            input.pendingValidationRules = validationRules[input.inputId]
+        })
     },
 }
 
