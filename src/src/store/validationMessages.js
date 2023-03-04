9a1b6bb34526e9e54beb7762cc2e49bab6a82cd7
neomn110 <neomn110@gmail.com>
2023-03-04 19:13:35 +0330

    feat: remove is from isRequired rule

--- src/src/store/validationMessages.js
+++ src/src/store/validationMessages.js
@@ -1,6 +1,6 @@
 import {validationRules} from "./validationRules.js";
 export const validationMessages = {
-    required: 'is required',
+    required: 'required',
     alpha: 'should be alphabetic',
     email: 'should be valid email address',
     numeric: 'should be numeric',
