f7b6f1563fa0f04be83cd590c2487f6b0c41bf59
neomn110 <neomn110@gmail.com>
2023-03-05 15:54:51 +0330

    style: adjust validatoinStatus.vue margin

--- src/src/components/WebsiteLogo.vue
+++ src/src/components/WebsiteLogo.vue
@@ -1,12 +1,14 @@
 <template>
   <div v-auto-animate class=" flex flex-col justify-center border-2 border-gray-900 rounded-2xl text-white bg-gray-800 overflow-hidden">
-    <div class="flex justify-center items-center ">
+    <div class="flex justify-center items-center">
       <label for="" class=" m-2 text-2xl">Easy</label>
       <img class="w-20 m-2" :src="icons.brand.logo">
     </div>
-    <ValidationStatus v-if="this.anyFocusedInput" class=" border m-2 grow"/>
+    <ValidationStatus v-if="this.anyFocusedInput" class=" m-[2px] mb-1 border border-gray-600 rounded grow "/>
   </div>
 </template>
+
+
 
 <script>
 import ValidationStatus from "./ValidationStatus.vue";
