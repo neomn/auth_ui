e5d44f13142a7eae9f3a53734b25eca31e932db8
neomn110 <neomn110@gmail.com>
2023-03-05 14:31:42 +0330

    feat: display formInput invalid ruels

--- src/src/components/ValidationStatus.vue
+++ src/src/components/ValidationStatus.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="rounded-lg">
-    {{this.focusedInput}}
+  <div class="rounded-lg overflow-y-scroll">
+    <div>
+<!--      {{this.validRulesForFocusedInput}}-->
+    </div>
+    <div class="text-red-400">
+      {{Object.keys(this.invalidRulesForFocusedInput)}}
+    </div>
   </div>
 </template>
 
@@ -10,7 +15,9 @@
 export default {
   name: "ValidationStatus",
   computed: {
-    focusedInput(){ return this.$store.getters.currentlyFocusedInput }
+    focusedInput(){ return this.$store.getters.currentlyFocusedInput },
+    validRulesForFocusedInput(){ return this.$store.getters.validRules(this.focusedInput) },
+    invalidRulesForFocusedInput(){ return this.$store.getters.invalidRules(this.focusedInput) },
   },
 }
 </script>
