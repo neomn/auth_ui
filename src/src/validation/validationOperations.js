80e982500c595d668f930939d4151cdd511ac5a7
neomn110 <neomn110@gmail.com>
2023-03-07 22:21:35 +0330

    refactor: comment out unused functions

--- src/src/validation/validationOperations.js
+++ src/src/validation/validationOperations.js
@@ -1,25 +1,26 @@
 import {store} from "../store/store.js";
 
+
 export const validationOperations = {
 
-    updateActionButtonVisibility() {
-        this.noFormInputIsVisible() ? this.setActionButtonVisibility(true): this.setActionButtonVisibility(false)
-    },
+    // updateActionButtonVisibility() {
+    //     this.noFormInputIsVisible() ? this.setActionButtonVisibility(true): this.setActionButtonVisibility(false)
+    // },
 
-    noFormInputIsVisible() {
-        return this.visibleFormInputs().length <= 0
-    },
+    // noFormInputIsVisible() {
+    //     return this.visibleFormInputs().length <= 0
+    // },
 
-    visibleFormInputs() {
-        const result = Object.values(store.getters.formInputs).map(formInput => {
-            return formInput.display ? formInput.inputId : null
-        })
-        return result.filter(item => item !== null)
-    },
+    // visibleFormInputs() {
+    //     const result = Object.values(store.getters.formInputs).map(formInput => {
+    //         return formInput.display ? formInput.inputId : null
+    //     })
+    //     return result.filter(item => item !== null)
+    // },
 
-    getInvalidFormInputs(v$) {
-        console.log(this.visibleFormInputs())
-    },
+    // getInvalidFormInputs(v$) {
+    //     console.log(this.visibleFormInputs())
+    // },
 
     pulseInvalidInputs(){
         store.commit('updatePulseInvalidInputs', true)
